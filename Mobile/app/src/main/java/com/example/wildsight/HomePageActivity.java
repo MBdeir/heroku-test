b9baf46f7ee44da8040aa7dcd0a5a13d9399c711
Bugs Bunny <jaa93@mail.aub.edu>
2023-11-15 19:26:18 +0200

    Implemented the Result page and fixed bugs related to icons in home page

---
+++ Mobile/app/src/main/java/com/example/wildsight/HomePageActivity.java
@@ -0,0 +1,15 @@
+package com.example.wildsight;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
