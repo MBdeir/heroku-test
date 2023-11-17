52776e43f5795131a04a98452f88e7570d554d16
Bugs Bunny <jaa93@mail.aub.edu>
2023-11-17 19:16:42 +0200

    Added support for opening the camera in the home page activity

--- Mobile/app/src/main/java/com/example/wildsight/HomePageActivity.java
+++ Mobile/app/src/main/java/com/example/wildsight/HomePageActivity.java
@@ -2,15 +2,30 @@ package com.example.wildsight;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePageActivity extends AppCompatActivity {
+    ImageView btnCam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        btnCam = findViewById(R.id.cameraBtn);
+        btnCam.setOnClickListener(view -> {
+            try{
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        });
+
+
     }
     public void DiscoverAnimals(View view){
         Intent intent = new Intent(this, AnimalsListActivity.class);
