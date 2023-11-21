903fb4e71d85a72d61f6837831610299bbfde362
Bugs Bunny <jaa93@mail.aub.edu>
2023-11-21 22:08:24 +0200

    finalized activity to fetch info about the result and display it in the result page

--- Mobile/app/src/main/java/com/example/wildsight/HomePageActivity.java
+++ Mobile/app/src/main/java/com/example/wildsight/HomePageActivity.java
@@ -1,9 +1,12 @@
 package com.example.wildsight;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -16,16 +19,21 @@ import android.Manifest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
 
 public class HomePageActivity extends AppCompatActivity {
-    ImageView btnCam,plusIcon;
+    ImageView btnCam, plusIcon;
+    private Dialog customProgressDialog;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int PICK_IMAGE = 2;
     private static final int PERMISSION_REQUEST_STORAGE = 1000;
@@ -52,6 +60,7 @@ public class HomePageActivity extends AppCompatActivity {
             }
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,12 +70,12 @@ public class HomePageActivity extends AppCompatActivity {
         plusIcon = findViewById(R.id.plusIcon);
 
         btnCam.setOnClickListener(view -> {
-            try{
+            try {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
                 }
-            }catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         });
@@ -84,23 +93,28 @@ public class HomePageActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            // here we are converting the bitmap to a file form since the api accepts file
             File imageFile = convertBitmapToFile("capturedImage", imageBitmap);
-            uploadImageToServer(imageFile);
-        }
-        else if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            String imagePath = uploadImageToServer(imageFile);
+            if (imagePath != null) {
+                openResultActivity(imagePath);
+            }
+        } else if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
             Uri selectedImageUri = data.getData();
             if (selectedImageUri != null) {
                 String imagePath = getPathFromUri(selectedImageUri);
                 if (imagePath != null) {
                     File imageFile = new File(imagePath);
-                    uploadImageToServer(imageFile);
+                    String uploadedImagePath = uploadImageToServer(imageFile);
+                    if (uploadedImagePath != null) {
+                        openResultActivity(uploadedImagePath);
+                    }
                 }
             }
         }
     }
+
     private String getPathFromUri(Uri uri) {
-        String[] projection = { MediaStore.Images.Media.DATA };
+        String[] projection = {MediaStore.Images.Media.DATA};
         Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
         if (cursor != null) {
             int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
@@ -111,8 +125,8 @@ public class HomePageActivity extends AppCompatActivity {
         }
         return null;
     }
+
     private File convertBitmapToFile(String fileName, Bitmap bitmap) {
-        // Create a file in the application's directory
         File file = new File(getExternalFilesDir(null), fileName + ".jpg");
         try {
             FileOutputStream out = new FileOutputStream(file);
@@ -125,7 +139,13 @@ public class HomePageActivity extends AppCompatActivity {
         return file;
     }
 
-    private void uploadImageToServer(File imageFile) {
+    private String uploadImageToServer(File imageFile) {
+
+        customProgressDialog = new Dialog(this);
+        customProgressDialog.setContentView(R.layout.custom_progress_dialog);
+        customProgressDialog.setCancelable(false);
+        customProgressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        customProgressDialog.show();
         String url = "https://wildsight.onrender.com/uploadImg";
 
         VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, url, imageFile,
@@ -138,22 +158,89 @@ public class HomePageActivity extends AppCompatActivity {
                             JSONObject prediction = jsonResponse.getJSONObject("prediction");
                             String animalClass = prediction.getString("class");
                             double confidence = prediction.getDouble("confidence");
-
-                            showToast("Detected: " + animalClass + " with confidence: " + confidence);
+                            customProgressDialog.dismiss();
+                            fetchAdditionalInfo(animalClass, imageFile.getAbsolutePath());
                         } else if (jsonResponse.has("error")) {
+                            customProgressDialog.dismiss();
                             String errorMessage = jsonResponse.getString("error");
                             showToast("Error: " + errorMessage);
                         }
                     } catch (Exception e) {
+                        customProgressDialog.dismiss();
                         e.printStackTrace();
                         showToast("Parsing error: " + e.getMessage());
                     }
                 },
                 error -> {
+                    customProgressDialog.dismiss();
                     showToast("Network error: " + error.getMessage());
                 });
 
         Volley.newRequestQueue(this).add(volleyMultipartRequest);
+
+        return null;
+    }
+
+    private void fetchAdditionalInfo(String animalName, String imagePath) {
+        customProgressDialog = new Dialog(this);
+        customProgressDialog.setContentView(R.layout.custom_progress_dialog);
+        customProgressDialog.setCancelable(false);
+        customProgressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        customProgressDialog.show();
+        String secondApiUrl = "https://wildsight.onrender.com/animal_info?name=" + animalName;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, secondApiUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResult = new JSONObject(response);
+                            JSONObject animalDetails = jsonResult.getJSONObject(animalName);
+
+                            String shortDescription = animalDetails.getString("shortDescription");
+                            String habitat = animalDetails.getString("habitat");
+                            String diet = animalDetails.getString("diet");
+                            String location = animalDetails.getString("location");
+                            String type = animalDetails.getString("type");
+                            String lifeSpan = animalDetails.getString("lifeSpan");
+                            String weight = animalDetails.getString("weight");
+                            String topSpeed = animalDetails.getString("top_speed");
+
+                            Intent intent = new Intent(HomePageActivity.this, ResultActivity.class);
+                            intent.putExtra("name", animalName);
+                            intent.putExtra("imagePath", imagePath);
+                            intent.putExtra("shortDescription", shortDescription);
+                            intent.putExtra("habitat", habitat);
+                            intent.putExtra("diet", diet);
+                            intent.putExtra("location", location);
+                            intent.putExtra("type", type);
+                            intent.putExtra("lifeSpan", lifeSpan);
+                            intent.putExtra("weight", weight);
+                            intent.putExtra("topSpeed", topSpeed);
+                            customProgressDialog.dismiss();
+                            startActivity(intent);
+                        } catch (JSONException e) {
+                            customProgressDialog.dismiss();
+                            e.printStackTrace();
+                            showToast("Error parsing additional info: " + e.getMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        customProgressDialog.dismiss();
+                        showToast("Error fetching additional info: " + error.getMessage());
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+    private void openResultActivity(String imagePath) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("imagePath", imagePath);
+        startActivity(intent);
     }
 
     private void showToast(String message) {
@@ -166,13 +253,12 @@ public class HomePageActivity extends AppCompatActivity {
         // This could be showing a dialog, updating a TextView, etc.
     }
 
-
-    public void DiscoverAnimals(View view){
+    public void DiscoverAnimals(View view) {
         Intent intent = new Intent(this, AnimalsListActivity.class);
         startActivity(intent);
     }
 
-    public void FunFacts(View view){
+    public void FunFacts(View view) {
         Intent intent = new Intent(this, FunFactsActivity.class);
         startActivity(intent);
     }
