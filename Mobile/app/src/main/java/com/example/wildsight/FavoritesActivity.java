8ce17a8afb9c0b7cd8a4eb554bd3cc6a264af0ff
MBdeir <moey.bdeir@gmail.com>
2023-11-21 16:26:26 +0200

    added fav activity

--- Mobile/app/src/main/java/com/example/wildsight/FavoritesActivity.java
+++ Mobile/app/src/main/java/com/example/wildsight/FavoritesActivity.java
@@ -7,7 +7,6 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -40,10 +39,15 @@ public class FavoritesActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.animals_list);
+        setContentView(R.layout.favorites);
         itemContainer = findViewById(R.id.itemContainer);
         requestQueue = Volley.newRequestQueue(this);
         fetchAnimals();
+    }
+
+    private String getSavedUsername() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        return sharedPreferences.getString("username", null); // Returns null if no username is found
     }
     private void fetchAnimals() {
         customProgressDialog = new Dialog(this);
@@ -51,47 +55,55 @@ public class FavoritesActivity extends AppCompatActivity{
         customProgressDialog.setCancelable(false);
         customProgressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         customProgressDialog.show();
+
         String url = "https://wildsight.onrender.com/fav_animals";
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
-        String username = sharedPreferences.getString("username", "");
 
-
-        JSONObject jsonBody = new JSONObject();
-        try {
-            jsonBody.put("username", username);
-        } catch (JSONException e) {
-            e.printStackTrace();
+        String username = getSavedUsername();
+        if (username == null) {
+            // Handle the case where username is not found
+            customProgressDialog.dismiss();
+            return;
         }
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, jsonBody,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray animalsArray = response.getJSONArray("animals");
-                            for (int i = 0; i < animalsArray.length(); i++) {
-                                JSONObject animal = animalsArray.getJSONObject(i);
-                                addAnimalToView(animal);
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        customProgressDialog.dismiss();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                    }
-                });
+        JSONObject jsonRequest = new JSONObject();
+        try {
+            jsonRequest.put("username", username);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            customProgressDialog.dismiss();
+            return;
+        }
 
-        requestQueue.add(jsonObjectRequest);
+        // Change to JsonArrayRequest
+        JsonArrayPostRequest jsonArrayPostRequest = new JsonArrayPostRequest(Request.Method.POST, url, jsonRequest, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject animal = response.getJSONObject(i);
+                        addAnimalToView(animal);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                customProgressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                customProgressDialog.dismiss();
+            }
+        });
+
+        requestQueue.add(jsonArrayPostRequest);
     }
+
+
 
     private void addAnimalToView(JSONObject animal) {
         try {
-            View itemView = getLayoutInflater().inflate(R.layout.item_layout_favourite, null);
+            View itemView = getLayoutInflater().inflate(R.layout.item_layout, null);
             final ImageView itemImage = itemView.findViewById(R.id.itemImage);
             TextView itemName = itemView.findViewById(R.id.itemName);
             TextView moreInfoButton = itemView.findViewById(R.id.moreInfoButton);
@@ -123,8 +135,8 @@ public class FavoritesActivity extends AppCompatActivity{
             e.printStackTrace();
         }
     }
-
-
-
-
+    public void GoToCategories(View view){
+        Intent intent = new Intent(this, AnimalsListActivity.class);
+        startActivity(intent);
+    }
 }
